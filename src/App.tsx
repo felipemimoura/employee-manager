f32b432f4b54a923151a075fdfc0386d13eed36f
Felipe Moura <felipemimoura@gmail.com>
2023-01-11 15:21:41 -0300

    chore: initial config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+import "./index.css";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 className="text-blue-900">Employer menager</h1>;
+}
+
+export default App;
